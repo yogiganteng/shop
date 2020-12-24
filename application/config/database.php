950a2e40e388a0b97d44aefc7ce282e11aea658b
yogimardilah <yogimardilah19@gmail.com>
2020-12-24 16:54:43 +0700

    test con

--- application/config/database.php
+++ application/config/database.php
@@ -91,7 +91,7 @@ $db['default'] = array(
 	'swap_pre' => '',
 	'encrypt' => FALSE,
 	'compress' => FALSE,
-	'stricton' => FALSE,
+	//'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
 );
