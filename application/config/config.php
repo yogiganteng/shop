eb2404653f8c1c04ac8ce84bab14e92d32326971
yogimardilah <yogimardilah19@gmail.com>
2020-12-29 17:01:47 +0700

    test

--- application/config/config.php
+++ application/config/config.php
@@ -36,7 +36,7 @@ $config['base_url'] = 'http://yogimardilah.com/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
